17549b9581fb39fe5a9514f30d570482cb27c097
Josean498 <alemanjr@hotmail.com>
2019-10-17 00:03:49 +0200

    Segundo dia, cambio de json y algo de css.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from  '@angular/common/http';
+import { map }  from  'rxjs/operators';
+ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+ 
+  coches: any;
+  
+  constructor(private httpClient: HttpClient) {
+    this.coches = this.httpClient.get('https://api.myjson.com/bins/pghre').pipe(map(res => res['results']));
+  }
 }
