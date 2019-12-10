fe6b08f34c3e1f860862bd5bdd6de32f09a5e054
Josean498 <alemanjr@hotmail.com>
2019-12-10 12:46:17 +0100

    Cambios en el ts, html y scss, realizo databinding y ngif. Finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { HttpClient } from  '@angular/common/http';
 import { map }  from  'rxjs/operators';
- 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
- 
+
   coches: any;
+
+  head: string = "Coches";
+  buscar: string = '';
   
   constructor(private httpClient: HttpClient) {
     this.coches = this.httpClient.get('https://api.myjson.com/bins/14gh4c').pipe(map(res => res['results']));
